0e1df298b5ee9ed46742c221ab8a647242243749
Evan Prodromou <evanp@socialwebfoundation.org>
2024-12-02 09:57:33 -0500

    style: move activitypub-actor-profile to its own module

---
+++ activitypub-actor-profile.js
@@ -0,0 +1,79 @@
+
+class ActivityPubActorProfile extends HTMLElement {
+  static get observedAttributes() {
+    return [
+      'name',
+      'webfinger',
+      'summary',
+      'url',
+      'icon'
+    ];
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+        <h2 class="name">(name)</h2>
+        <p class="webfinger">(webfinger)</p>
+        <div class="summary">(summary)</div>
+        <p><a class="url" href="javascript:void(0)">(url)</a></p>
+      `;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'name':
+        this.shadowRoot.querySelector('.name').textContent = newValue;
+        break;
+      case 'webfinger':
+        this.shadowRoot.querySelector('.webfinger').textContent = newValue;
+        break;
+      case 'summary':
+        this.shadowRoot.querySelector('.summary').innerHTML = newValue;
+        break;
+      case 'url':
+        this.shadowRoot.querySelector('.url').textContent = newValue;
+        this.shadowRoot.querySelector('.url').href = newValue;
+        break;
+      case 'icon':
+        this.shadowRoot.querySelector('.icon').src = newValue;
+        break;
+    }
+  }
+
+  get name() {
+    return this.getAttribute('name');
+  }
+
+  set name(value) {
+    this.setAttribute('name', value);
+  }
+
+  get summary() {
+    return this.getAttribute('summary');
+  }
+
+  set summary(value) {
+    this.setAttribute('summary', value);
+  }
+
+  get url() {
+    return this.getAttribute('url');
+  }
+
+  set url(value) {
+    this.setAttribute('url', value);
+  }
+
+  get webfinger() {
+    return this.getAttribute('webfinger');
+  }
+
+  set webfinger(value) {
+    this.setAttribute('webfinger', value);
+  }
+}
+
+// Define the custom element
+customElements.define('activitypub-actor-profile', ActivityPubActorProfile);
