2643d30e89917f54611556ab80cf3a4e5733461c
Evan Prodromou <evanp@socialwebfoundation.org>
2024-12-02 09:21:21 -0500

    feat: webfinger in output

--- activitypub-actor.js
+++ activitypub-actor.js
@@ -1,14 +1,14 @@
 // Usage: <activitypub-actor webfinger="webfinger:example@domain"></activitypub-actor>
 // Description: Fetches the actor object from the given webfinger and displays the name, summary, and url of the actor.
 
-class ActivityPubActor extends HTMLElement {
+class ActivityPubActorProfile extends HTMLElement {
   static get observedAttributes() {
     return [
-      'webfinger',
-      'actor-id',
       'name',
+      'webfinger',
       'summary',
-      'url'
+      'url',
+      'icon'
     ];
   }
 
@@ -17,8 +17,76 @@ class ActivityPubActor extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
         <h2 id="name">(name)</h2>
+        <p id="webfinger">(webfinger)</p>
         <div id="summary">(summary)</div>
         <p><a id="url" href="javascript:void(0)">(url)</a></p>
+      `;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'name':
+        this.shadowRoot.getElementById('name').textContent = newValue;
+        break;
+      case 'webfinger':
+        this.shadowRoot.getElementById('webfinger').textContent = newValue;
+        break;
+      case 'summary':
+        this.shadowRoot.getElementById('summary').innerHTML = newValue;
+        break;
+      case 'url':
+        this.shadowRoot.getElementById('url').textContent = newValue;
+        this.shadowRoot.getElementById('url').href = newValue;
+        break;
+      case 'icon':
+        this.shadowRoot.getElementById('icon').src = newValue;
+        break;
+    }
+  }
+
+  get name() {
+    return this.getAttribute('name');
+  }
+
+  set name(value) {
+    this.setAttribute('name', value);
+  }
+
+  get summary() {
+    return this.getAttribute('summary');
+  }
+
+  set summary(value) {
+    this.setAttribute('summary', value);
+  }
+
+  get url() {
+    return this.getAttribute('url');
+  }
+
+  set url(value) {
+    this.setAttribute('url', value);
+  }
+}
+
+// Define the custom element
+customElements.define('activitypub-actor-profile', ActivityPubActorProfile);
+
+class ActivityPubActor extends HTMLElement {
+  static get observedAttributes() {
+    return [
+      'webfinger',
+      'actor-id'
+    ];
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+        <div>
+          <activitypub-actor-profile id="profile"></activitypub-actor-profile>
+        </div>
       `;
   }
 
@@ -29,17 +97,6 @@ class ActivityPubActor extends HTMLElement {
         break;
       case 'actor-id':
         this.updateActorId(newValue);
-        break;
-      case 'name':
-        this.shadowRoot.getElementById('name').textContent = newValue;
-        break;
-      case 'summary':
-        this.shadowRoot.getElementById('summary').innerHTML = newValue;
-        break;
-      case 'url':
-        this.shadowRoot.getElementById('url').textContent = newValue;
-        this.shadowRoot.getElementById('url').href = newValue;
-        break;
     }
   }
 
@@ -70,10 +127,12 @@ class ActivityPubActor extends HTMLElement {
       })
       const json = await res.json();
 
-      this.name = json.name;
-      this.summary = json.summary;
-      this.url = json.url;
+      const profile = this.shadowRoot.getElementById('profile');
 
+      profile.name = json.name;
+      profile.summary = json.summary;
+      profile.url = json.url;
+      profile.webfinger = this.webfinger;
     } catch (err) {
       console.error(err)
     }
@@ -93,30 +152,6 @@ class ActivityPubActor extends HTMLElement {
 
   set actorId(value) {
     this.setAttribute('actor-id', value);
-  }
-
-  get name() {
-    return this.getAttribute('name');
-  }
-
-  set name(value) {
-    this.setAttribute('name', value);
-  }
-
-  get summary() {
-    return this.getAttribute('summary');
-  }
-
-  set summary(value) {
-    this.setAttribute('summary', value);
-  }
-
-  get url() {
-    return this.getAttribute('url');
-  }
-
-  set url(value) {
-    this.setAttribute('url', value);
   }
 }
 
