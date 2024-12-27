8a21a4ebb091f66a1726705079a2c02ae5cc3c6c
Evan Prodromou <evanp@socialwebfoundation.org>
2024-12-26 19:35:31 -0800

    fix: correct url for avatar icon

--- avatar-icon.js
+++ avatar-icon.js
@@ -1,79 +1,41 @@
 // Usage: <avatar-icon url="https://example.com/avatar.jpg" size="128"></avatar-icon>
 // Displays a square avatar or a gray square if missing
 
-class AvatarIcon extends HTMLElement {
-  static get observedAttributes() {
-    return ['url', 'size'];
+import { html, css, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+
+class AvatarIcon extends LitElement {
+
+  static get properties() {
+    return {
+      'url': { type: String },
+      'size': { type: String }
+    }
   }
+
+  static styles = css`
+  :host {
+    display: inline-block;
+  }
+  .avatar {
+      background-color: lightgray;
+      background-size: cover;
+      background-position: center;
+      border-radius: 8px;
+  }
+  `;
 
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
-
-    // Initialize shadow DOM
-    this.shadowRoot.innerHTML = `
-          <style>
-              :host {
-                  display: inline-block;
-              }
-              .avatar {
-                  width: var(--avatar-size, 128px);
-                  height: var(--avatar-size, 128px);
-                  background-color: lightgray;
-                  background-size: cover;
-                  background-position: center;
-                  border-radius: 8px;
-              }
-          </style>
-          <div class="avatar"></div>
-      `;
+    this.size = 128;
   }
 
-  connectedCallback() {
-    // Initialize appearance when added to the DOM
-    this.updateAvatar();
-    this.updateSize();
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'url') {
-      this.updateAvatar();
-    } else if (name === 'size') {
-      this.updateSize();
-    }
-  }
-
-  updateAvatar() {
-    const avatarElement = this.shadowRoot.querySelector('.avatar');
-    const url = this.getAttribute('url');
-
-    if (url) {
-      avatarElement.style.backgroundImage = `url(${url})`;
-    } else {
-      avatarElement.style.backgroundImage = 'none';
-    }
-  }
-
-  updateSize() {
-    const size = this.getAttribute('size') || '128'; // Default to 128px
-    const avatarElement = this.shadowRoot.querySelector('.avatar');
-    avatarElement.style.setProperty('--avatar-size', `${size}px`);
-  }
-
-  get url() {
-    return this.getAttribute('url');
-  }
-
-  set url(value) {
-    this.setAttribute('url', value);
-  }
-
-  get size() {
-    return this.getAttribute('size');
-  }
-
-  set size(value) {
-    this.setAttribute('size', value);
+  render() {
+    return html`
+      <div
+        class="avatar"
+        style="width: ${this.size}px; height: ${this.size}px; background-image: url(${this.url});">
+      </div>
+    `;
   }
 }
 
