785f048a7c0e49777b1917b1e3e3f96819575e16
Evan Prodromou <evanp@socialwebfoundation.org>
2025-01-03 20:06:47 -0800

    feat: add <infinite-scroll> to ap-collection; closes #5

--- ap-collection.js
+++ ap-collection.js
@@ -1,6 +1,7 @@
 
 import { ActivityPubElement } from './ap-element.js';
 import { html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+import 'https://cdn.jsdelivr.net/npm/infinite-scroll-component/dist/infinite-scroll.min.js';
 import { ActivityPubObject } from './ap-object.js';
 
 export class ActivityPubCollectionElement extends ActivityPubElement {
@@ -12,6 +13,7 @@ export class ActivityPubCollectionElement extends ActivityPubElement {
       ...super.properties,
       pageSize: { type: Number, attribute: 'page-size' },
       _items: { type: Array, state: true },
+      _next: { type: String, state: true }
     }
   }
 
@@ -42,20 +44,26 @@ export class ActivityPubCollectionElement extends ActivityPubElement {
     } else {
       const cls = this.constructor._itemElement;
       return html`
-        <ol class="items">
-        ${this._items?.map(item => {
-        const el = new cls();
-        if (typeof item === 'string') {
-          el.objectId = item;
-        } else {
-          el.json = item;
-        }
-        return html`
-          <li class="item">
-          ${el}
-          </li>
-          `})}
-        </ol>
+
+        <infinite-scroll
+          @infinite-scroll-fetch=${this.fetchMore}
+            >
+          <ol class="items">
+            ${this._items?.map(item => {
+              const el = new cls();
+              if (typeof item === 'string') {
+                el.objectId = item;
+              } else {
+                el.json = item;
+              }
+              return html`
+              <li class="item">
+              ${el}
+              </li>
+              `
+            })}
+          </ol>
+        </infinite-scroll>
       `;
     }
   }
@@ -69,13 +77,14 @@ export class ActivityPubCollectionElement extends ActivityPubElement {
 
   async fetchItems() {
     const items = [];
+    let next = null;
 
     if (this.json.items) {
       items.push(...this.json.items);
     } else if (this.json.orderedItems) {
       items.push(...this.json.orderedItems);
     } else if (this.json.first) {
-      let next = this.json.first;
+      next = this.json.first;
       while (next &&
         items.length < this.pageSize) {
         const res = await this.constructor.fetchFunction(next, {
@@ -95,6 +104,34 @@ export class ActivityPubCollectionElement extends ActivityPubElement {
       }
     }
 
+    this._next = next;
     this._items = items;
+  }
+
+  async fetchMore() {
+    const items = [];
+    let next = this._next;
+    if (!next) {
+      return;
+    }
+    while (next &&
+      items.length < this.pageSize) {
+      const res = await this.constructor.fetchFunction(next, {
+        headers: { Accept: this.constructor.MEDIA_TYPES.join(', ') }
+      });
+      if (!res.ok) {
+        console.error('Failed to fetch collection page', res);
+        break;
+      }
+      const page = await res.json();
+      if (page.items) {
+        items.push(...page.items);
+      } else if (page.orderedItems) {
+        items.push(...page.orderedItems);
+      }
+      next = page.next;
+    }
+    this._next = next;
+    this._items = [...this._items, ...items];
   }
 }
