09699e48724f99477fbcd1b9532ba654c4e6abfd
Evan Prodromou <evan@openearth.org>
2025-05-30 00:45:38 -0400

    fix: imports in index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import { ActivityPubActivityCollection } from 'lib/ap-activity-collection.js'
-import { ActivityPubActivity } from 'lib/ap-activity.js'
-import { ActivityPubActorCollection } from 'lib/ap-actor-collection.js'
-import { ActivityPubActorItem } from 'lib/ap-actor-item.js'
-import { ActivityPubActorPage } from 'lib/ap-actor-page.js'
-import { ActivityPubActorProfile } from 'lib/ap-actor-profile.js'
-import { ActivityPubAnnounceActivity } from 'lib/ap-announce-activity.js'
-import { ActivityPubArticle } from 'lib/ap-article.js'
-import { ActivityPubCollection } from 'lib/ap-collection.js'
-import { ActivityPubCreateActivity } from 'lib/ap-create-activity.js'
-import { ActivityPubElement } from 'lib/ap-element.js'
-import { ActivityPubNote } from 'lib/ap-note.js'
-import { ActivityPubObjectCollection } from 'lib/ap-object-collection.js'
-import { ActivityPubObject } from 'lib/ap-object.js'
-import { AvatarIcon } from 'lib/avatar-icon.js'
+import { ActivityPubActivityCollection } from './lib/ap-activity-collection.js'
+import { ActivityPubActivity } from './lib/ap-activity.js'
+import { ActivityPubActorCollection } from './lib/ap-actor-collection.js'
+import { ActivityPubActorItem } from './lib/ap-actor-item.js'
+import { ActivityPubActorPage } from './lib/ap-actor-page.js'
+import { ActivityPubActorProfile } from './lib/ap-actor-profile.js'
+import { ActivityPubAnnounceActivity } from './lib/ap-announce-activity.js'
+import { ActivityPubArticle } from './lib/ap-article.js'
+import { ActivityPubCollectionElement } from './lib/ap-collection.js'
+import { ActivityPubCreateActivity } from './lib/ap-create-activity.js'
+import { ActivityPubElement } from './lib/ap-element.js'
+import { ActivityPubNote } from './lib/ap-note.js'
+import { ActivityPubObjectCollection } from './lib/ap-object-collection.js'
+import { ActivityPubObject } from './lib/ap-object.js'
+import { AvatarIcon } from './lib/avatar-icon.js'
 
 export {
   ActivityPubActivityCollection,
@@ -23,7 +23,7 @@ export {
   ActivityPubActorProfile,
   ActivityPubAnnounceActivity,
   ActivityPubArticle,
-  ActivityPubCollection,
+  ActivityPubCollectionElement,
   ActivityPubCreateActivity,
   ActivityPubElement,
   ActivityPubNote,
